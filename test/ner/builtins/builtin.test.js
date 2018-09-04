0f47dd802402b064c54d0a6a6c4ff9b42e90d6bb
Jesus Seijas <jseijas@gmail.com>
2018-09-05 01:24:25 +0200

    feat: Name entity extraction for currency, date, date interval, email, ip, hashtag, dimensions,

--- test/ner/builtins/builtin.test.js
+++ test/ner/builtins/builtin.test.js
@@ -4,13 +4,28 @@ const numberTests = require('./number.json');
 const numberOrdinalTests = require('./number-ordinal.json');
 const numberPercentTests = require('./number-percent.json');
 const numberCurrency = require('./number-currency.json');
+const numberDimension = require('./number-dimension.json');
+const sequence = require('./sequence.json');
+const date = require('./date.json');
 
 expect.extend({
   toContainResolution(received, argument) {
     for (let i = 0; i < received.length; i += 1) {
       const actual = received[i];
       if (actual.resolution) {
-        const pass = this.equals(actual.resolution, argument);
+        const keys = Object.keys(argument);
+        let pass = true;
+        for (let j = 0; j < keys.length; j += 1) {
+          const key = keys[j];
+          if (!actual.resolution[key]) {
+            pass = false;
+          }
+          if (argument[key] !== '*') {
+            if (!this.equals(actual.resolution[key], argument[key])) {
+              pass = false;
+            }
+          }
+        }
         if (pass) {
           return {
             message: () => (`expected ${this.utils.printReceived(received)} not to contain resolution ${this.utils.printExpected(argument)}`),
@@ -33,6 +48,20 @@ function addTests(base, locale) {
   const manager = new NlpManager({ languages: [locale] });
   for (let i = 0; i < base.length; i += 1) {
     const testCase = base[i];
+    const keys = Object.keys(testCase);
+    for (let j = 0; j < keys.length; j += 1) {
+      const key = keys[j];
+      if (key.startsWith('result')) {
+        const current = testCase[key];
+        const currentKeys = Object.keys(current);
+        for (let k = 0; k < currentKeys.length; k += 1) {
+          const currentKey = currentKeys[k];
+          if ((currentKey.includes('date') || currentKey.includes('Date')) && testCase[key][currentKey].length === 24) {
+            testCase[key][currentKey] = new Date(testCase[key][currentKey]);
+          }
+        }
+      }
+    }
     if (!testCase.avoid || !testCase.avoid.includes(locale)) {
       const utterance = testCase[utteranceName] || testCase.utterance;
       if (utterance) {
@@ -52,7 +81,7 @@ const languages = [
   { locale: 'pt', name: 'Portuguese' },
 ];
 
-describe('NerManager Number builtins', () => {
+describe('NER Manager builtins', () => {
   languages.forEach((language) => {
     describe(`Numbers ${language.name}`, () => {
       addTests(numberTests, language.locale);
@@ -68,6 +97,15 @@ describe('NerManager Number builtins', () => {
     });
     describe(`Currency ${language.name}`, () => {
       addTests(numberCurrency, language.locale);
+    });
+    describe(`Dimension ${language.name}`, () => {
+      addTests(numberDimension, language.locale);
+    });
+    describe(`Sequence ${language.name}`, () => {
+      addTests(sequence, language.locale);
+    });
+    describe(`Date ${language.name}`, () => {
+      addTests(date, language.locale);
     });
   });
 });
