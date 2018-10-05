2c2496275dee5a98df029a5aed5da14c3eab9aa2
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-10-05 19:34:11 +0400

    Add await for training bot

--- examples/console-bot/index.js
+++ examples/console-bot/index.js
@@ -33,30 +33,32 @@ function say(message) {
   console.log(message);
 }
 
-trainnlp(nlpManager, say);
-say('Say something!');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false,
-});
-rl.on('line', async line => {
-  if (line.toLowerCase() === 'quit') {
-    rl.close();
-    process.exit();
-  } else {
-    const result = await nlpManager.process(line);
-    console.log(result);
-    const answer =
-      result.score > threshold && result.answer
-        ? result.answer
-        : "Sorry, I don't understand";
-    let sentiment = '';
-    if (result.sentiment.score !== 0) {
-      sentiment = `  ${result.sentiment.score > 0 ? ':)' : ':('}   (${
-        result.sentiment.score
-      })`;
+(async () => {
+  await trainnlp(nlpManager, say);
+  say('Say something!');
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+  });
+  rl.on('line', async line => {
+    if (line.toLowerCase() === 'quit') {
+      rl.close();
+      process.exit();
+    } else {
+      const result = await nlpManager.process(line);
+      console.log(result);
+      const answer =
+        result.score > threshold && result.answer
+          ? result.answer
+          : "Sorry, I don't understand";
+      let sentiment = '';
+      if (result.sentiment.score !== 0) {
+        sentiment = `  ${result.sentiment.score > 0 ? ':)' : ':('}   (${
+          result.sentiment.score
+        })`;
+      }
+      say(`bot> ${answer}${sentiment}`);
     }
-    say(`bot> ${answer}${sentiment}`);
-  }
-});
+  });
+})();
