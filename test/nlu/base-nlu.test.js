5beb1275bff371e247f5d3ff51d174e2c71d7545
Jesus Seijas <jesus.seijas@axa-groupsolutions.com>
2019-04-07 15:10:11 +0200

    fix: Improve BaseNLU tests

--- test/nlu/base-nlu.test.js
+++ test/nlu/base-nlu.test.js
@@ -21,17 +21,16 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-const BaseNlu = require('../../lib/nlu/base-nlu');
-const { NlpUtil } = require('../../lib');
+const { BaseNLU, NlpUtil } = require('../../lib');
 
 describe('Base NLU', () => {
   describe('constructor', () => {
     test('Should create a new instance', () => {
-      const nlu = new BaseNlu();
+      const nlu = new BaseNLU();
       expect(nlu).toBeDefined();
     });
     test('Should initialize the default properties', () => {
-      const nlu = new BaseNlu();
+      const nlu = new BaseNLU();
       expect(nlu.language).toEqual('en');
       expect(nlu.stemmer).toBeDefined();
       expect(nlu.docs).toEqual([]);
@@ -39,25 +38,25 @@ describe('Base NLU', () => {
     });
     test('I can provide my own stemmer', () => {
       const stemmer = NlpUtil.getStemmer('en');
-      const nlu = new BaseNlu({ stemmer });
+      const nlu = new BaseNLU({ stemmer });
       expect(nlu.stemmer).toBe(stemmer);
     });
     test('I can decide to not keep the stopwords', () => {
-      const nlu = new BaseNlu({ keepStopwords: false });
+      const nlu = new BaseNLU({ keepStopwords: false });
       expect(nlu.keepStopwords).toBeFalsy();
     });
   });
 
   describe('add', () => {
     test('Should add an utterance and intent', () => {
-      const nlu = new BaseNlu({ language: 'fr' });
+      const nlu = new BaseNLU({ language: 'fr' });
       nlu.add('Bonjour', 'greet');
       expect(nlu.docs).toHaveLength(1);
       nlu.add('bonne nuit', 'greet');
       expect(nlu.docs).toHaveLength(2);
     });
     test('Should add an utterance and intent even to different intents', () => {
-      const nlu = new BaseNlu({ language: 'fr' });
+      const nlu = new BaseNLU({ language: 'fr' });
       nlu.add('Bonjour', 'greet');
       expect(nlu.docs).toHaveLength(1);
       nlu.add('bonne nuit', 'greet');
@@ -68,26 +67,26 @@ describe('Base NLU', () => {
       expect(nlu.docs).toHaveLength(4);
     });
     test('Should check that the utterance is an string', () => {
-      const nlu = new BaseNlu({ language: 'fr' });
+      const nlu = new BaseNLU({ language: 'fr' });
       expect(() => nlu.add(1, 'greet')).toThrow('Utterance must be an string');
       expect(() => nlu.add(undefined, 'greet')).toThrow(
         'Utterance must be an string'
       );
     });
     test('Should check that the intent is an string', () => {
-      const nlu = new BaseNlu({ language: 'fr' });
+      const nlu = new BaseNLU({ language: 'fr' });
       expect(() => nlu.add('Bonjour', 1)).toThrow('Intent must be an string');
       expect(() => nlu.add('Bonjour', undefined)).toThrow(
         'Intent must be an string'
       );
     });
     test('If the utterance is empty, do not add doc to the classifier', () => {
-      const nlu = new BaseNlu({ language: 'fr' });
+      const nlu = new BaseNLU({ language: 'fr' });
       nlu.add(' ', 'greet');
       expect(nlu.docs).toHaveLength(0);
     });
     test('If the utterance was already added, do not add doc to the classifier', () => {
-      const nlu = new BaseNlu({ language: 'fr' });
+      const nlu = new BaseNLU({ language: 'fr' });
       nlu.add('Bonjour', 'greet');
       expect(nlu.docs).toHaveLength(1);
       nlu.add('Bonjour', 'greet');
@@ -97,20 +96,20 @@ describe('Base NLU', () => {
 
   describe('Pos utterance', () => {
     test('Should return the position of the utterance', () => {
-      const nlu = new BaseNlu({ language: 'fr' });
+      const nlu = new BaseNLU({ language: 'fr' });
       nlu.add('Bonjour', 'greet');
       nlu.add('bonne nuit', 'greet');
       expect(nlu.posUtterance('Bonjour')).toEqual(0);
       expect(nlu.posUtterance('Bonne nuit')).toEqual(1);
     });
     test('Should return -1 if the utterance does not exists', () => {
-      const nlu = new BaseNlu({ language: 'fr' });
+      const nlu = new BaseNLU({ language: 'fr' });
       nlu.add('Bonjour', 'greet');
       nlu.add('bonne nuit', 'greet');
       expect(nlu.posUtterance('Bonsoir')).toEqual(-1);
     });
     test('Should return position if the utterance stems already exists', () => {
-      const nlu = new BaseNlu({ language: 'fr' });
+      const nlu = new BaseNLU({ language: 'fr' });
       nlu.add('Bonjour', 'greet');
       nlu.add('bonne nuit', 'greet');
       expect(nlu.posUtterance('Bonn nuite')).toEqual(1);
@@ -119,7 +118,7 @@ describe('Base NLU', () => {
 
   describe('exists utterance', () => {
     test('Should return if the utterance exists', () => {
-      const nlu = new BaseNlu({ language: 'fr' });
+      const nlu = new BaseNLU({ language: 'fr' });
       nlu.add('Bonjour', 'greet');
       nlu.add('bonne nuit', 'greet');
       expect(nlu.existsUtterance('Bonjour')).toBeTruthy();
@@ -130,7 +129,7 @@ describe('Base NLU', () => {
 
   describe('remove utterance', () => {
     test('Should remove the utterance if no intent is given', () => {
-      const nlu = new BaseNlu({ language: 'fr' });
+      const nlu = new BaseNLU({ language: 'fr' });
       nlu.add('Bonjour', 'greet');
       nlu.add('Bonjour François', 'greet');
       nlu.add('bonne nuit', 'greet');
@@ -138,44 +137,44 @@ describe('Base NLU', () => {
       expect(nlu.docs).toHaveLength(2);
     });
     test('Should do nothing if utterance does not exists', () => {
-      const nlu = new BaseNlu({ language: 'fr' });
+      const nlu = new BaseNLU({ language: 'fr' });
       nlu.add('Bonjour', 'greet');
       nlu.add('bonne nuit', 'greet');
       nlu.remove('bonsoire');
       expect(nlu.docs).toHaveLength(2);
     });
     test('Should remove the utterance if intent is provided', () => {
-      const nlu = new BaseNlu({ language: 'fr' });
+      const nlu = new BaseNLU({ language: 'fr' });
       nlu.add('Bonjour', 'greet');
       nlu.add('bonne nuit', 'greet');
       nlu.remove('bonjour', 'greet');
       expect(nlu.docs).toHaveLength(1);
     });
     test('Should do nothing if utterance does not exists for the intent', () => {
-      const nlu = new BaseNlu({ language: 'fr' });
+      const nlu = new BaseNLU({ language: 'fr' });
       nlu.add('Bonjour', 'greet');
       nlu.add('bonne nuit', 'greet');
       nlu.remove('Bonjour', 'meh');
       expect(nlu.docs).toHaveLength(2);
     });
     test('Should do nothing if utterance is empty', () => {
-      const nlu = new BaseNlu({ language: 'fr' });
+      const nlu = new BaseNLU({ language: 'fr' });
       nlu.add('Bonjour', 'greet');
       nlu.add('bonne nuit', 'greet');
       nlu.remove('', 'meh');
       expect(nlu.docs).toHaveLength(2);
     });
     test('Should throw an error if the utterance is not a string', () => {
-      const nlu = new BaseNlu({ language: 'fr' });
+      const nlu = new BaseNLU({ language: 'fr' });
       expect(() => {
-        nlu.remove([], 'meh');
+        nlu.remove(1, 'meh');
       }).toThrow('Utterance must be an string');
     });
   });
 
   describe('Is equal classification', () => {
     test('It should return true if all the classifications are 0.5', () => {
-      const nlu = new BaseNlu();
+      const nlu = new BaseNLU();
       const actual = nlu.isEqualClassification([
         { value: 0.5 },
         { value: 0.5 },
@@ -184,13 +183,22 @@ describe('Base NLU', () => {
       expect(actual).toBeTruthy();
     });
     test('It should return false if not all the classifications are 0.5', () => {
-      const nlu = new BaseNlu();
+      const nlu = new BaseNLU();
       const actual = nlu.isEqualClassification([
         { value: 0.5 },
         { value: 0.4 },
         { value: 0.5 },
       ]);
       expect(actual).toBeFalsy();
+    });
+  });
+
+  describe('Normalize Neural', () => {
+    test('If called with 0 total, do not calculate', () => {
+      const nlu = new BaseNLU();
+      const expected = [{ label: 'a', value: 0 }, { label: 'b', value: 0 }];
+      const actual = nlu.normalizeNeural(expected);
+      expect(actual).toBe(expected);
     });
   });
 });
