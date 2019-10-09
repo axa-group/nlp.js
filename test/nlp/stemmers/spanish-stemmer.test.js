7fc66aa01169f29272428bbf1712ecde38243311
Dani Pinyol <dpinol@gmail.com>
2019-10-09 13:06:35 +0200

    fix: remove accents from SpanishStemmer
    added unit test for Spanish.
    "trepadora" and "considerara" are still not correctly stemmed

---
+++ test/nlp/stemmers/spanish-stemmer.test.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) AXA Group Operations Spain S.A.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+const { NlpUtil } = require('../../../lib');
+
+describe('Spanish Stemmer', () => {
+  describe('Constructor', () => {
+    test('It should create a new instance', () => {
+      const stemmer = NlpUtil.getStemmer('gl');
+      expect(stemmer).toBeDefined();
+    });
+  });
+  describe('Stem', () => {
+    test('Should tokenize and stem spanish text', () => {
+      const text =
+        'Me compraría una gato trepadora perfectamente si lo considerara aconsejable';
+      const stemmer = NlpUtil.getStemmer('es');
+      const actual = stemmer.tokenizeAndStem(text);
+      const expected = [
+        'me',
+        'compr',
+        'una',
+        'gat',
+        'trepador', // TODO should be trep
+        'perfect',
+        'si',
+        'lo',
+        'considerar', // TODO should be consider
+        'aconsej',
+      ];
+      expect(actual).toEqual(expected);
+    });
+  });
+});
