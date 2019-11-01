cbc327ca0415b37498376dc8a1cd98688102c20c
Jesus Seijas <jesus.seijas@axa.com>
2019-11-01 12:37:46 +0100

    feat: Sentiment fi

--- packages/lang-fi/src/index.js
+++ packages/lang-fi/src/index.js
@@ -26,6 +26,7 @@ const TokenizerFi = require('./tokenizer-fi');
 const StemmerFi = require('./stemmer-fi');
 const StopwordsFi = require('./stopwords-fi');
 const NormalizerFi = require('./normalizer-fi');
+const SentimentFi = require('./sentiment/sentiment_fi');
 
 module.exports = {
   LangFi,
@@ -33,4 +34,5 @@ module.exports = {
   StopwordsFi,
   TokenizerFi,
   NormalizerFi,
+  SentimentFi,
 };
---
+++ packages/lang-fi/src/sentiment/sentiment_fi.js
@@ -0,0 +1,11 @@
+const afinn = require('./afinn_fi.json');
+const pattern = require('./pattern_fi.json');
+const senticon = require('./senticon_fi.json');
+const negations = require('./negations_fi.json');
+
+module.exports = {
+  afinn,
+  pattern,
+  senticon,
+  negations,
+};
