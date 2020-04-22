49c6dbe4302109ab362bb9f7869f1334fa81a54c
David Villace Hernandez <david.villace.sopra@axa.com>
2020-04-22 14:32:22 +0200

    patch: corrections due to result of PR

--- examples/07-nlpjs-on-aws-lambda/nlpjs-lambda/hello-world/tests/unit/test-handler.js
+++ examples/07-nlpjs-on-aws-lambda/nlpjs-lambda/hello-world/tests/unit/test-handler.js
@@ -1,22 +1,22 @@
-'use strict';
-
-const app = require('../../app.js');
 const chai = require('chai');
-const expect = chai.expect;
-var event, context;
+const app = require('../../app.js');
+
+const { expect } = chai;
+let event;
+let context;
 
 describe('Tests index', function () {
-    it('verifies successful response', async () => {
-        const result = await app.lambdaHandler(event, context)
+  it('verifies successful response', async () => {
+    const result = await app.lambdaHandler(event, context);
 
-        expect(result).to.be.an('object');
-        expect(result.statusCode).to.equal(200);
-        expect(result.body).to.be.an('string');
+    expect(result).to.be.an('object');
+    expect(result.statusCode).to.equal(200);
+    expect(result.body).to.be.an('string');
 
-        let response = JSON.parse(result.body);
+    const response = JSON.parse(result.body);
 
-        expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("hello world");
-        // expect(response.location).to.be.an("string");
-    });
+    expect(response).to.be.an('object');
+    expect(response.message).to.be.equal('hello world');
+    // expect(response.location).to.be.an("string");
+  });
 });
