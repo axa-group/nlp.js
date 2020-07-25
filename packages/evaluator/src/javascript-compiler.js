22d6908e63f57ba65e0bd8df7d8e98dd71661af1
Jesus Seijas <jesus.seijas@axa.com>
2020-07-25 18:04:51 +0200

    fix: don't return same value in several returns

--- packages/evaluator/src/javascript-compiler.js
+++ packages/evaluator/src/javascript-compiler.js
@@ -474,13 +474,11 @@ class JavascriptCompiler {
     const newContext = context;
     if ({}.hasOwnProperty.call(context, node.name)) {
       context[node.name] = value;
-      return value;
-    }
-    if (context.input && {}.hasOwnProperty.call(context.input, node.name)) {
+    } else if (context.input && {}.hasOwnProperty.call(context.input, node.name)) {
       context.input[node.name] = value;
-      return value;
+    } else {
+      newContext[node.name] = value;
     }
-    newContext[node.name] = value;
     return value;
   }
 
