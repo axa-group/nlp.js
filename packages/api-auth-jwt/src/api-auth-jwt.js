06c3d92db54bb84ae500c150e5b3a010f8df9c69
Nick Freear <nfreear@yahoo.co.uk>
2020-08-01 16:07:50 +0100

    fix: cannot read property of undefined at ApiAuthJwt.register

--- packages/api-auth-jwt/src/api-auth-jwt.js
+++ packages/api-auth-jwt/src/api-auth-jwt.js
@@ -29,18 +29,20 @@ const getSettings = require('./default-settings');
 
 class ApiAuthJwt {
   register(container) {
-    const settings = getSettings(container);
-    const database = container.get('database');
-    const apiServer = container.get('api-server');
-    configurePassport(database, settings);
-    const plugin = passport.initialize();
-    apiServer.plugins.push(plugin);
-    const logger = container.get('logger');
-    container.register('auth', { ensureAuthenticated });
-    const router = apiServer.newRouter();
-    mountUser(router, container);
-    apiServer.routers.push(router);
-    logger.info('API Auth JWT plugin mounted');
+    process.nextTick(() => {
+      const settings = getSettings(container);
+      const database = container.get('database');
+      const apiServer = container.get('api-server');
+      configurePassport(database, settings);
+      const plugin = passport.initialize();
+      apiServer.plugins.push(plugin);
+      const logger = container.get('logger');
+      container.register('auth', { ensureAuthenticated });
+      const router = apiServer.newRouter();
+      mountUser(router, container);
+      apiServer.routers.push(router);
+      logger.info('API Auth JWT plugin mounted');
+    });
   }
 }
 
