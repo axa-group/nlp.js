1d8f02cd8dfceaca6e589dccb733cdc345a166ba
Jesus Seijas <jesus.seijas@axa.com>
2020-10-11 12:18:35 +0200

    docs: measure corpus use NLP threshold

--- examples/13-languages/measure-corpus.js
+++ examples/13-languages/measure-corpus.js
@@ -26,7 +26,7 @@ const { Nlp } = require('../../packages/nlp/src');
 // const { containerBootstrap } = require('@nlpjs/core');
 // const { Nlp } = require('@nlpjs/nlp');
 
-async function measureCorpus(corpus, plugins, showErrors = true) {
+async function measureCorpus(corpus, plugins) {
   const container = await containerBootstrap();
   container.use(Nlp);
   if (plugins) {
@@ -40,31 +40,34 @@ async function measureCorpus(corpus, plugins, showErrors = true) {
   await nlp.addCorpus(corpus);
   await nlp.train();
   let total = 0;
-  let good = 0;
-  let perfect = 0;
+  let goodThreshold = 0;
+  let goodNoThreshold = 0;
   for (let i = 0; i < corpus.data.length; i += 1) {
     const item = corpus.data[i];
     for (let j = 0; j < item.tests.length; j += 1) {
       const test = item.tests[j];
       total += 1;
-      const result = await nlp.process(test);
+      nlp.settings.threshold = 0;
+      let result = await nlp.process(test);
       if (result.intent === item.intent) {
-        good += 1;
-        if (result.score >= 0.5) {
-          perfect += 1;
-        }
-      } else if (showErrors) {
-        console.log(
-          `Expected: ${item.intent} Received: ${result.intent} "${test}"`
-        );
+        goodNoThreshold += 1;
+      }
+      nlp.settings.threshold = 0.5;
+      result = await nlp.process(test);
+      if (result.intent === item.intent) {
+        goodThreshold += 1;
       }
     }
   }
   console.log(`Total tests executed: ${total}`);
-  console.log(`Total intents guessed correctly: ${good}`);
-  console.log(`Total intents guessed correctly with score >= 0.5: ${perfect}`);
-  console.log(`Precision without threshold: ${(good * 100) / total}%`);
-  console.log(`Precision with threshold: ${(perfect * 100) / total}%`);
+  console.log(`Total intents guessed correctly: ${goodNoThreshold}`);
+  console.log(
+    `Total intents guessed correctly with score >= 0.5: ${goodThreshold}`
+  );
+  console.log(
+    `Precision without threshold: ${(goodNoThreshold * 100) / total}%`
+  );
+  console.log(`Precision with threshold: ${(goodThreshold * 100) / total}%`);
 }
 
 module.exports = measureCorpus;
