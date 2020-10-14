e654062a498a3b9e218646592a1dd6d5548c4729
Raimon Ràfols <raimon.rafols@gmail.com>
2020-10-15 00:23:28 +0100

    fix: linting

--- examples/12-similarity/09-similarity-benchmark.js
+++ examples/12-similarity/09-similarity-benchmark.js
@@ -34,67 +34,85 @@ function getTime(hrTime) {
 }
 
 function runWordTests(leven) {
-    const t0 = process.hrtime();
-    
-    leven('potatoe', 'potatoe');
-    leven('', '123');
-    leven('123', '');
-    leven('a', 'b');
-    leven('ab', 'ac');
-    leven('abc', 'axc');
-    leven('xabxcdxxefxgx', '1ab2cd34ef5g6');
-    leven('xabxcdxxefxgx', 'abcdefg');
-    leven('javawasneat', 'scalaisgreat');
-    leven('example', 'samples');
-    leven('forward', 'drawrof');
-    leven('sturgeon', 'urgently');
-    leven('levenshtein', 'frankenstein');
-    leven('distance', 'difference');
-    leven('distance', 'eistancd');
-    leven('你好世界', '你好');
-    leven('因為我是中國人所以我會說中文', '因為我是英國人所以我會說英文');
-    leven('mikailovitch', 'Mikhaïlovitch');
+  const t0 = process.hrtime();
 
-    const tEllapsed = process.hrtime(t0);
-    return getTime(tEllapsed);
+  leven('potatoe', 'potatoe');
+  leven('', '123');
+  leven('123', '');
+  leven('a', 'b');
+  leven('ab', 'ac');
+  leven('abc', 'axc');
+  leven('xabxcdxxefxgx', '1ab2cd34ef5g6');
+  leven('xabxcdxxefxgx', 'abcdefg');
+  leven('javawasneat', 'scalaisgreat');
+  leven('example', 'samples');
+  leven('forward', 'drawrof');
+  leven('sturgeon', 'urgently');
+  leven('levenshtein', 'frankenstein');
+  leven('distance', 'difference');
+  leven('distance', 'eistancd');
+  leven('你好世界', '你好');
+  leven('因為我是中國人所以我會說中文', '因為我是英國人所以我會說英文');
+  leven('mikailovitch', 'Mikhaïlovitch');
+
+  const tEllapsed = process.hrtime(t0);
+  return getTime(tEllapsed);
 }
 
 function runLongTests(leven) {
-    const t0 = process.hrtime();
+  const t0 = process.hrtime();
 
-    const text1 = 'Morbi interdum ultricies neque varius condimentum. Donec volutpat turpis interdum metus ultricies vulputate. Duis ultricies rhoncus sapien, sit amet fermentum risus imperdiet vitae. Ut et lectus';
-    const text2 = 'Duis erat dolor, cursus in tincidunt a, lobortis in odio. Cras magna sem, pharetra et iaculis quis, faucibus quis tellus. Suspendisse dapibus sapien in justo cursus';
-    leven(text1, text2);
+  const text1 =
+    'Morbi interdum ultricies neque varius condimentum. Donec volutpat turpis interdum metus ultricies vulputate. Duis ultricies rhoncus sapien, sit amet fermentum risus imperdiet vitae. Ut et lectus';
+  const text2 =
+    'Duis erat dolor, cursus in tincidunt a, lobortis in odio. Cras magna sem, pharetra et iaculis quis, faucibus quis tellus. Suspendisse dapibus sapien in justo cursus';
+  leven(text1, text2);
 
-    const tEllapsed = process.hrtime(t0);
-    return getTime(tEllapsed);
+  const tEllapsed = process.hrtime(t0);
+  return getTime(tEllapsed);
 }
 
 function runVeryLongTests(leven) {
-    const t0 = process.hrtime();
+  const t0 = process.hrtime();
 
-    let text1 = ''
-    let text2 = ''
-    for (let i = 0; i < 50; i++) {
-        text1 += 'Morbi interdum ultricies neque varius condimentum. Donec volutpat turpis interdum metus ultricies vulputate. Duis ultricies rhoncus sapien, sit amet fermentum risus imperdiet vitae. Ut et lectus';
-        text2 += 'Duis erat dolor, cursus in tincidunt a, lobortis in odio. Cras magna sem, pharetra et iaculis quis, faucibus quis tellus. Suspendisse dapibus sapien in justo cursus';
-    }
-    
-    leven(text1, text2);
+  let text1 = '';
+  let text2 = '';
+  for (let i = 0; i < 50; i += 1) {
+    text1 +=
+      'Morbi interdum ultricies neque varius condimentum. Donec volutpat turpis interdum metus ultricies vulputate. Duis ultricies rhoncus sapien, sit amet fermentum risus imperdiet vitae. Ut et lectus';
+    text2 +=
+      'Duis erat dolor, cursus in tincidunt a, lobortis in odio. Cras magna sem, pharetra et iaculis quis, faucibus quis tellus. Suspendisse dapibus sapien in justo cursus';
+  }
 
-    const tEllapsed = process.hrtime(t0);
-    return getTime(tEllapsed);
+  leven(text1, text2);
+
+  const tEllapsed = process.hrtime(t0);
+  return getTime(tEllapsed);
 }
-
 
 for (let i = 0; i < levenAlgs.length; i += 1) {
-    const leven = levenAlgs[i];
+  const leven = levenAlgs[i];
 
-    execTimeWord[i] = runWordTests(leven);
-    execTimeLong[i] = runLongTests(leven);
-    execTimeVeryLong[i] = runVeryLongTests(leven);
+  execTimeWord[i] = runWordTests(leven);
+  execTimeLong[i] = runLongTests(leven);
+  execTimeVeryLong[i] = runVeryLongTests(leven);
 }
 
-console.log('Web Assembly vs JavaScript benchmark on words:', execTimeWord[1], ' vs ', execTimeWord[0]);
-console.log('Web Assembly vs JavaScript benchmark on long texts:', execTimeLong[1], ' vs ', execTimeLong[0]);
-console.log('Web Assembly vs JavaScript benchmark on very long texts:', execTimeVeryLong[1], ' vs ', execTimeVeryLong[0]);
+console.log(
+  'Web Assembly vs JavaScript benchmark on words:',
+  execTimeWord[1],
+  ' vs ',
+  execTimeWord[0]
+);
+console.log(
+  'Web Assembly vs JavaScript benchmark on long texts:',
+  execTimeLong[1],
+  ' vs ',
+  execTimeLong[0]
+);
+console.log(
+  'Web Assembly vs JavaScript benchmark on very long texts:',
+  execTimeVeryLong[1],
+  ' vs ',
+  execTimeVeryLong[0]
+);
