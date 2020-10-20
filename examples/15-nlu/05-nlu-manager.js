1257f9d2b7e8f8221b0ddce7d317fe4b1aca352e
Jesus Seijas <jesus.seijas@axa.com>
2020-10-20 18:55:37 +0200

    docs: Document nlpjs/nlu

---
+++ examples/15-nlu/05-nlu-manager.js
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) AXA Group Operations Spain S.A.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+const { containerBootstrap } = require('../../packages/core/src');
+const { NluManager, NluNeural } = require('../../packages/nlu/src');
+const { LangEn } = require('../../packages/lang-en/src');
+const { LangEs } = require('../../packages/lang-es/src');
+// const { containerBootstrap } = require('@nlpjs/core');
+// const { NluManager, NluNeural } = require('@nlpjs/nlu');
+// const { LangEn } = require('@nlpjs/lang-en');
+// const { LangEs } = require('@nlpjs/lang-es');
+
+function addFoodDomainEn(manager) {
+  manager.assignDomain('en', 'order.check', 'food');
+  manager.add('en', 'what do I have in my basket', 'order.check');
+  manager.add('en', 'check my cart', 'order.check');
+  manager.add('en', "show me what I've ordered", 'order.check');
+  manager.add('en', "what's in my basket", 'order.check');
+  manager.add('en', 'check my order', 'order.check');
+  manager.add('en', 'check what I have ordered', 'order.check');
+  manager.add('en', 'show my order', 'order.check');
+  manager.add('en', 'check my basket', 'order.check');
+
+  manager.assignDomain('en', 'order.check_status', 'food');
+  manager.add('en', 'how soon will it be delivered', 'order.check_status');
+  manager.add('en', 'check the status of my delivery', 'order.check_status');
+  manager.add('en', 'when should I expect delivery', 'order.check_status');
+  manager.add('en', 'check my order status', 'order.check_status');
+  manager.add('en', 'where is my order', 'order.check_status');
+  manager.add('en', 'where is my delivery', 'order.check_status');
+  manager.add('en', 'status of my order', 'order.check_status');
+}
+
+function addFoodDomainEs(manager) {
+  manager.assignDomain('es', 'order.check', 'food');
+  manager.add('es', 'qué tengo en mi cesta', 'order.check');
+  manager.add('es', 'comprueba mi carrito', 'order.check');
+  manager.add('es', 'enséñame qué he pedido', 'order.check');
+  manager.add('es', 'qué hay en mi carrito?', 'order.check');
+  manager.add('es', 'comprueba mi compra', 'order.check');
+  manager.add('es', 'comprueba qué he comprado', 'order.check');
+  manager.add('es', 'muéstrame mi compra', 'order.check');
+
+  manager.assignDomain('es', 'order.check_status', 'food');
+  manager.add('es', 'cuándo me lo van a traer?', 'order.check_status');
+  manager.add('es', 'cómo va la entrega?', 'order.check_status');
+  manager.add('es', 'cuándo me traerán mi pedido?', 'order.check_status');
+  manager.add('es', 'en qué estado está mi pedido?', 'order.check_status');
+  manager.add('es', 'dónde está mi compra?', 'order.check_status');
+  manager.add('es', 'dónde está mi pedido?', 'order.check_status');
+  manager.add('es', 'estado de mi compra', 'order.check_status');
+}
+
+function addPersonalityDomainEn(manager) {
+  manager.assignDomain('en', 'agent.acquaintance', 'personality');
+  manager.add('en', 'say about you', 'agent.acquaintance');
+  manager.add('en', 'why are you here', 'agent.acquaintance');
+  manager.add('en', 'what is your personality', 'agent.acquaintance');
+  manager.add('en', 'describe yourself', 'agent.acquaintance');
+  manager.add('en', 'tell me about yourself', 'agent.acquaintance');
+  manager.add('en', 'tell me about you', 'agent.acquaintance');
+  manager.add('en', 'what are you', 'agent.acquaintance');
+  manager.add('en', 'who are you', 'agent.acquaintance');
+  manager.add('en', 'talk about yourself', 'agent.acquaintance');
+
+  manager.assignDomain('en', 'agent.age', 'personality');
+  manager.add('en', 'your age', 'agent.age');
+  manager.add('en', 'how old is your platform', 'agent.age');
+  manager.add('en', 'how old are you', 'agent.age');
+  manager.add('en', "what's your age", 'agent.age');
+  manager.add('en', "I'd like to know your age", 'agent.age');
+  manager.add('en', 'tell me your age', 'agent.age');
+}
+
+function addPersonalityDomainEs(manager) {
+  manager.assignDomain('es', 'agent.acquaintance', 'personality');
+  manager.add('es', 'cuéntame sobre ti', 'agent.acquaintance');
+  manager.add('es', 'qué haces aquí?', 'agent.acquaintance');
+  manager.add('es', 'cómo es tu personalidad?', 'agent.acquaintance');
+  manager.add('es', 'descríbete', 'agent.acquaintance');
+  manager.add('es', 'quién eres?', 'agent.acquaintance');
+  manager.add('es', 'qué eres?', 'agent.acquaintance');
+  manager.add('es', 'háblame de ti', 'agent.acquaintance');
+
+  manager.assignDomain('es', 'agent.age', 'personality');
+  manager.add('es', 'qué edad tienes?', 'agent.age');
+  manager.add('es', 'cuántos años tienes?', 'agent.age');
+  manager.add('es', 'cuál es tu edad?', 'agent.age');
+  manager.add('es', 'quiero saber tu edad', 'agent.age');
+  manager.add('es', 'dime tu edad', 'agent.age');
+}
+
+// const manager = new NluManager({ container, locales: ['en', 'es'], useNoneFeature: false, trainByDomain: true });
+(async () => {
+  const container = await containerBootstrap();
+  container.use(LangEn);
+  container.use(LangEs);
+  container.use(NluNeural);
+  const manager = new NluManager({ container, locales: ['en', 'es'] });
+  addFoodDomainEn(manager);
+  addFoodDomainEs(manager);
+  addPersonalityDomainEn(manager);
+  addPersonalityDomainEs(manager);
+  await manager.train();
+  // You can provide the locale of the language
+  let actual = await manager.process('es', 'dime quién eres tú');
+  console.log(actual);
+  // If the locale is not provided, then the language is guessed
+  actual = await manager.process('dime quién eres tú');
+  console.log(actual);
+  // {
+  //   locale: 'es',
+  //   utterance: 'dime quién eres tú',
+  //   domain: 'personality',
+  //   languageGuessed: true,
+  //   localeIso2: 'es',
+  //   language: 'Spanish',
+  //   nluAnswer: {
+  //     classifications: [ [Object], [Object] ],
+  //     entities: undefined,
+  //     explanation: undefined
+  //   },
+  //   classifications: [
+  //     { intent: 'agent.acquaintance', score: 0.8700343801471809 },
+  //     { intent: 'agent.age', score: 0.12996561985281913 }
+  //   ],
+  //   intent: 'agent.acquaintance',
+  //   score: 0.8700343801471809
+  // }
+})();
--- examples/15-nlu/README.md
+++ examples/15-nlu/README.md
@@ -20,3 +20,19 @@ In this example, all intents and domains are trained in a single model.
 ## 04 domain-manager-by-domain
 Shows the usage of the _DomainManager_ class training by domain, that means that every domain is trained in a separate model, and there exists a master model that classifies an utterance into a domain.
 This is done by providing the setting _trainByDomain_ with value _true_ at the constructor of _DomainManager_
+
+## 05 nlu-manager
+Shows the usage of the _NluManager_ class.
+The _NluManager_ is able to contain several _DomainManager_ instances, one per language.
+That way the structure is that _NluManager_ contains several languages, each language has a _DomainManager_ that is able to manage several domains per language.
+Also, _NluManager_ is able to process sentences without specifying the language, and internally it guess the language.
+
+## 06 nlu-manager-by-domain
+Shows the usage of the _NluManager_ class training by domain.
+It's the same example as the previous one but training by domain. That means that 6 models will be trained:
+- English personality
+- English food
+- English master (classify into domain personality or food)
+- Spanish personality
+- Spanish food
+- Spanish master (classify into domain personality or food)
