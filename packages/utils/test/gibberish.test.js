013503b88153b0f338437e0c8c404bf036a3a006
Jesus Seijas <jesus.seijas@axa.com>
2021-01-07 20:51:39 +0100

    fix: gibberish detection, no deviation of vowel over consonant

--- packages/utils/test/gibberish.test.js
+++ packages/utils/test/gibberish.test.js
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) AXA Group Operations Spain S.A.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 const { isGibberish } = require('../src');
 
 describe('Gibberish', () => {
@@ -10,7 +33,15 @@ describe('Gibberish', () => {
       expect(isGibberish('sure')).toBeFalsy();
       expect(isGibberish('very much')).toBeFalsy();
       expect(isGibberish('it feels so good')).toBeFalsy();
+      expect(
+        isGibberish(
+          'Qué tiene la zarzamora que a todas horas llora que llora por los rincones'
+        )
+      ).toBeFalsy();
+      expect(isGibberish('are you a male?')).toBeFalsy();
+      expect(isGibberish('are you a female?')).toBeFalsy();
     });
+
     test('Should return true for gibberish sentences', () => {
       expect(isGibberish('zxcvwerjasc')).toBeTruthy();
       expect(isGibberish('ertrjiloifdfyyoiu')).toBeTruthy();
