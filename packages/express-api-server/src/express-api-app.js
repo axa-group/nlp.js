4d7319258d68aac5cac4245b0c7c1f3182f92fa7
Eric Lara <eric.lara@axa.com>
2021-02-17 21:07:48 +0100

    update: simplify initialize method in express api app

--- packages/express-api-server/src/express-api-app.js
+++ packages/express-api-server/src/express-api-app.js
@@ -42,12 +42,20 @@ class ExpressApiApp {
     this.app.use(cors());
     this.app.use(express.urlencoded({ extended: false }));
     this.app.use(express.json());
+
+    this.loadComplements();
+    
+    return this.app;
+  }
+
+  loadComplements() {
     for (let i = 0; i < this.plugins.length; i += 1) {
-      this.app.use(this.plugins[i]);
+      const plugin = this.plugins[i];
+      logger.debug(`Loading plugin: ${plugin.name}`);
+      this.app.use(plugin);
     }
     if (this.settings.serveBot) {
-      const clientPath =
-        this.settings.clientPath || path.join(__dirname, './public');
+      const clientPath = this.settings.clientPath || path.join(__dirname, './public');
       logger.debug(`Serving bot client (path: ${clientPath}`);
       this.app.use(express.static(clientPath));
     }
@@ -57,7 +65,6 @@ class ExpressApiApp {
       logger.debug(`Loading custom router: ${JSON.stringify(routes, null, 2)}`);
       this.app.use(this.settings.apiRoot, router);
     }
-    return this.app;
   }
 }
 
