032d4093e44516bc3e1a26d880d1c9fb587e1181
Deep Chakraborty <chakraborty.deep013@gmail.com>
2023-02-16 10:07:21 +0530

    Update packages/ner/src/extractor-trim.js
    Co-authored-by: Ingo Fischer <github@fischer-ka.de>

--- packages/ner/src/extractor-trim.js
+++ packages/ner/src/extractor-trim.js
@@ -55,7 +55,7 @@ class ExtractorTrim {
     const result = [];
     let matchFound;
     if (typeof condition.regex === 'string') {
-      if(condition.regex.startsWith('/'))
+      if (condition.regex.startsWith('/'))
         condition.regex = condition.regex.slice(1);
       var regexArr = condition.regex.split('/');
       var flag = regexArr.splice(-1);
