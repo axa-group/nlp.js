1e05fdf4bf3b2e67f7406e51e3917ce870a1cd6b
Anders D. Johnson <Anders.Johnson@target.com>
2023-05-18 22:47:05 -0500

    fix(ner): deserialize regex from SON
    Fixes a bug with Named Entity Recognition where it doesn't deserialize regular expressions from JSON properly for all rule types.

--- packages/ner/src/ner.js
+++ packages/ner/src/ner.js
@@ -517,11 +517,13 @@ class Ner extends Clonable {
       const entityKeys = Object.keys(json.rules[rKey]);
 
       entityKeys.forEach((eKey) => {
-        if (json.rules[rKey][eKey].type === 'regex') {
-          json.rules[rKey][eKey].rules = json.rules[rKey][eKey].rules.map(
-            (rule) => Ner.str2regex(rule)
-          );
-        }
+        json.rules[rKey][eKey].rules =
+          json.rules[rKey][eKey].type === 'regex'
+            ? json.rules[rKey][eKey].rules.map((rule) => Ner.str2regex(rule))
+            : json.rules[rKey][eKey].rules.map((rule) => ({
+                ...rule,
+                regex: Ner.str2regex(rule.regex),
+              }));
       });
     });
 
